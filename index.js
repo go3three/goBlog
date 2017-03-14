dfc37198b56bb163fcf641c9b24b82ab599d72af
Mohammed Naji <moha.buy@gmail.com>
2017-03-14 12:24:34 +0200

    fix files

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
 'use strict';
-const Hapi = require('hapi');
-const server = new Hapi.Server();
-server.connection({
-    port: process.env.PORT || 3000
-});
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function(request, reply) {
-        reply('Hello, world!');
-    }
-});
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function(request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-server.start((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
-});
+const route = require('./route.js');
+route();
