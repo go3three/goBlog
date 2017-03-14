dd7bac308731486ca5656441610b98914c6d7fe9
Mohammed Naji <moha.buy@gmail.com>
2017-03-14 11:55:18 +0200

    some fixes with heroku

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({
-    port: process.env.PORT || 3000,
-    host:'0.0.0.0'
-
+    port: process.env.PORT || 3000
 });
 server.start((err) => {
     if (err) {
